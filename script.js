4619deb1b8a413aeb60ed911c2d194da13f98cd7
Hashmi <digiacomo93@gmail.com>
2024-08-22 22:03:02 +0200

    Fix problem

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const confirmBtn = document.querySelector('#confirmBtn');
 let name = document.querySelector('#name');
 let author = document.querySelector('#author');
 let year = document.querySelector('#year');
+let pages = document.querySelector('#pages');
 let genre = document.querySelector('#genre');
 let status = document.querySelector('#status');
 const container = document.querySelector('.container-books');
@@ -14,10 +15,11 @@ function showDialog() {
     dialog.showModal();
 }
 
-function Book(name, author, year, genre, status) {
+function Book(name, author, year, pages, genre, status) {
     this.name = name;
     this.author = author;
     this.year = year;
+    this.pages = pages;
     this.genre = genre;
     this.status = status;
 }
@@ -26,24 +28,42 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
+function createBookList(book) {
 
-confirmBtn.addEventListener('click', () => {
+}
 
-    const newBook = new Book(name.value, author.value, year.value, genre.value, status.value);
-    addBookToLibrary(newBook);
-    console.log(myLibrary);
-
+function createElementCard(book) {
+    //Card
     const card = document.createElement('div');
     card.classList.add('card');
     container.appendChild(card);
-    card.style['width'] = '200px';
-    card.style['backgroundColor'] = 'lightgrey';
-    card.style['color'] = 'red';
+
+    //Ul
     const ul = document.createElement('ul');
-    card.appendChild(ul);
-    for (const key in newBook) {
+    card.appendChild(ul)
+
+    //Li
+    for (const key in book) {
         const li = document.createElement('li');
-        li.textContent = key.charAt(0).toUpperCase() + key.slice(1) + ': ' + newBook[key];
+        li.textContent = key.charAt(0).toUpperCase() + key.slice(1) + ': ' + book[key];
         ul.appendChild(li);
     }
+
+    //Buttons
+    const statusBtn = document.createElement('button');
+    statusBtn.classList.add('status-btn');
+    statusBtn.textContent = 'Change Status';
+    card.appendChild(statusBtn);
+    statusBtn.addEventListener('click', () => {
+    }
+    )
+
+
+}
+
+confirmBtn.addEventListener('click', () => {
+    const newBook = new Book(name.value, author.value, year.value, pages.value, genre.value, status.value);
+    addBookToLibrary(newBook);
+    createElementCard(newBook);
 })
+
