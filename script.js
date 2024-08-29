a587152fb48e89955aadda4ec4579f71dab53a66
Hashmi <digiacomo93@gmail.com>
2024-08-29 06:41:57 +0200

    Add event listeners

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
 const dialog = document.querySelector('dialog');
+const form = document.querySelector('form');
 const formInput = document.querySelectorAll('.formInput');
 const openBtn = document.querySelector('#openBtn');
 const closeBtn = document.querySelector('#closeBtn');
-const confirmBtn = document.querySelector('#confirmBtn');
+const container = document.querySelector('.container-books');
 let name = document.querySelector('#name');
 let author = document.querySelector('#author');
 let year = document.querySelector('#year');
 let pages = document.querySelector('#pages');
 let genre = document.querySelector('#genre');
 let status = document.querySelector('#status');
-const container = document.querySelector('.container-books');
 const myLibrary = [];
+
 
 function showDialog() {
     dialog.showModal();
+
 }
 
 function resetDialog() {
     formInput.forEach(input => {
         input.value = '';
     });
+}
+
+function closeDialog() {
     dialog.close();
+    resetDialog();
+
 }
 
 function Book(name, author, year, pages, genre, status) {
@@ -55,9 +62,11 @@ function createElementCard(book) {
     }
 
     //Change status Button
-    const statusBtn = document.createElement('button');
+    const statusBtn = document.createElement('input');
     statusBtn.classList.add('status-btn');
-    statusBtn.textContent = 'Change Status';
+    statusBtn.type = 'button';
+    statusBtn.value = 'Change Status';
+    statusBtn.style.marginRight = '0.5em';
     card.appendChild(statusBtn);
     statusBtn.addEventListener('click', () => {
         const statusLi = card.querySelector('ul > li:last-child');
@@ -72,9 +81,10 @@ function createElementCard(book) {
     })
 
     //Remove book Button
-    const removeBtn = document.createElement('button')
+    const removeBtn = document.createElement('input')
     removeBtn.classList.add('remove-btn');
-    removeBtn.textContent = 'Remove Book';
+    removeBtn.type = 'button';
+    removeBtn.value = 'Remove Book';
     card.appendChild(removeBtn);
     removeBtn.addEventListener('click', () => {
         myLibrary.splice(myLibrary.indexOf(book), 1);
@@ -82,10 +92,20 @@ function createElementCard(book) {
     })
 }
 
-confirmBtn.addEventListener('click', () => {
+openBtn.addEventListener('click', showDialog);
+
+closeBtn.addEventListener('click', closeDialog);
+
+form.addEventListener('submit', () => {
     const newBook = new Book(name.value, author.value, year.value, pages.value, genre.value, status.value);
     addBookToLibrary(newBook);
     createElementCard(newBook);
-    resetDialog();
+    closeDialog();
+})
+
+dialog.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+        closeDialog();
+    }
 })
 
